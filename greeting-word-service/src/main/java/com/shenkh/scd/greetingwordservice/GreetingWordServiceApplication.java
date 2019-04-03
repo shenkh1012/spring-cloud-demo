52826ffe321b4bced2f4ce1e55b393f33433b84c
Kyle Shen <fogogo@126.com>
2019-04-03 23:32:11 +0800

    Micro services apps

---
+++ greeting-word-service/src/main/java/com/shenkh/scd/greetingwordservice/GreetingWordServiceApplication.java
@@ -0,0 +1,11 @@
+package com.shenkh.scd.greetingwordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingWordServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GreetingWordServiceApplication.class, args);
+  }
+}
