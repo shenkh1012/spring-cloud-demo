a5f400246ef4ccfe58f890e0139ec8f072eb8baf
Kyle Shen <kyle.shen@delphi-tech.com>
2019-04-04 16:22:13 +0800

    Start 0.4.0

--- greeting-service/src/main/java/com/shenkh/scd/greetingservice/GreetingWordServiceAdaptor.java
+++ greeting-service/src/main/java/com/shenkh/scd/greetingservice/GreetingWordServiceAdaptor.java
@@ -1,7 +1,12 @@
 package com.shenkh.scd.greetingservice;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * <p>(C) 2003 Delphi Technology, inc. (dti)</p>
@@ -18,18 +23,34 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 public class GreetingWordServiceAdaptor {
-    private static final String GREETING_WORD_SERVICE_ENDPOINT = "http://localhost:8002";
-    private RestTemplate restTemplate;
+    private static final Logger l = Logger.getLogger(GreetingController.class.getName());
 
-    public GreetingWordServiceAdaptor(RestTemplate restTemplate) {
+    private static final String GREETING_WORD_SERVICE_NAME = "GREETING-WORD-SERVICE";
+    private RestTemplate restTemplate;
+    private EurekaClient discoveryClient;
+
+    public GreetingWordServiceAdaptor(RestTemplate restTemplate, EurekaClient discoveryClient) {
         this.restTemplate = restTemplate;
-    }
+        this.discoveryClient = discoveryClient;
+}
 
     public String getGreetingWord() {
-        return this.restTemplate.getForObject(GREETING_WORD_SERVICE_ENDPOINT, String.class);
+        return this.restTemplate.getForObject(serviceUri(), String.class);
     }
 
     public String getGreetingWord(String languageCode) {
-        return this.restTemplate.getForObject(GREETING_WORD_SERVICE_ENDPOINT + "/" + languageCode, String.class);
+        String uri = serviceUri() + languageCode;
+        l.info("Greeting Word: " + uri);
+
+        return this.restTemplate.getForObject(uri, String.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    private String serviceUri() {
+        List<InstanceInfo> list = discoveryClient.getApplication(GREETING_WORD_SERVICE_NAME).getInstances();
+        if (list != null && list.size() > 0 ) {
+            return list.get(0).getHomePageUrl();
+        }
+        return null;
     }
 }
